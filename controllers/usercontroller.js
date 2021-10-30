b82ac8ae876550a97e99329dba8ee112cc9c4a36
LA-Miller <lamiller516@gmail.com>
2021-10-30 13:16:41 -0400

    challenge 1 bronze complete

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -3,6 +3,34 @@ const router = express.Router();
 const { User } = require("../models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+// const { UniqueConstraintError } = require("sequelize/types");
 
+router.post("/create", async (req, res) => {
+
+    let { username, password } = req.body.user;
+    try {
+        const newUser = await User.create({
+            username,
+            password
+        });
+
+        res.status(200).json({
+            message: "User successfully saved",
+            user: newUser
+        })
+    }   catch (err) {
+            res.status(500).json({
+                message: "Failed to save User",
+            });
+        }
+})
+
+router.post("/login", async (req, res) => {
+    let { username, password } = req.body.user;
+
+    try {
+        
+    }
+})
 
 module.exports = router;
--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require('sequelize');
 
-const db = new Sequelize("postgres://postgres:YOURPASSWORDHERE@localhost:5432/animal-server");
+const db = new Sequelize("postgres://postgres:bc464deb64c94c749bb485515af91f4d@localhost:5432/animal-server");
 
 module.exports = db;
